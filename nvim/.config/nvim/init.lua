484919bce8087754698d3005cbd22bc54cd36d2a
林大中 <lindazhong@lindazhongs-MacBook-Air.local>
2023-03-26 00:48:57 +0800

    add init.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,21 @@
+require("core.options")
+require("core.keymaps")
+require("plugins-setup")
+require("core.colorscheme")
+-- plugins
+require("plugins.nvim-cmp")
+require("plugins.lsp") -- require the plugins.lsp.init
+require("plugins.lualine")
+require("plugins.telescope")
+require("plugins.project")
+require("plugins.treesitter")
+require("plugins.autopairs")
+require("plugins.indentline")
+require("plugins.which-key")
+require("plugins.gitsigns")
+require("plugins.bufferline")
+require("plugins.toggleterm")
+require("plugins.nvim-tree")
+require("plugins.alpha")
+require("plugins.symbols-outline") -- class outline
+require("plugins.navic")
