f9ab029f27b99a1445766740d5b231c3ec21e018
林大中 <lindazhong@lindazhongs-MacBook-Air.local>
2023-04-01 17:38:44 +0800

    remove used code

--- nvim/.config/nvim/lua/plugins/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/lsp/null-ls.lua
@@ -17,7 +17,7 @@ require("mason-null-ls").setup({
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
-local completion = null_ls.builtins.completion
+-- local completion = null_ls.builtins.completion
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
