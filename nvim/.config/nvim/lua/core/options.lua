4646da7be8b4b8f9ae32f76e160b39e1c866f502
林大中 <lindazhong@lindazhongs-MacBook-Air.local>
2023-04-02 20:20:22 +0800

    refomate and add python virtual-env detect

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,39 +1,53 @@
-local opt = vim.opt
+local options = {
+	-- line number
+	relativenumber = false,
+	number = true,
 
--- line number
-opt.relativenumber = false
-opt.number = true
+	-- indentation
+	tabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	autoindent = true,
 
--- indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
+	-- line wrapping
+	wrap = false,
 
--- line wrapping
-opt.wrap = false
+	-- search settings
+	ignorecase = true,
+	smartcase = true,
+	hlsearch = true, -- highlight search
 
--- search settings
-opt.ignorecase = true
-opt.smartcase = true
-opt.hlsearch = true -- highlight search
+	-- cursor line
+	cursorline = true,
 
--- cursor line
-opt.cursorline = true
+	-- appearance
+	termguicolors = true,
+	signcolumn = "yes",
 
--- appearance
-opt.termguicolors = true
-opt.signcolumn = "yes"
+	-- typing
+	undofile = true, -- can persistent undo
 
--- typing
-vim.opt.undofile = true -- can persistent undo
+	-- split windows
+	splitbelow = true,
+	splitright = true,
 
--- split windows
-opt.splitbelow = true
-opt.splitright = true
+	-- clipboard
+	clipboard = "unnamedplus",
 
--- clipboard
-opt.clipboard = "unnamedplus"
+	-- mouse
+	mouse = "a",
+}
 
--- mouse
-vim.opt.mouse = "a"
+for option, val in pairs(options) do
+	vim.opt[option] = val
+end
+
+-- python provider
+local conda_prefix = os.getenv("CONDA_PREFIX")
+if conda_prefix ~= nil then
+	vim.g.python_host_prog = conda_prefix .. "/bin/python"
+	vim.g.python3_host_prog = conda_prefix .. "/bin/python"
+else
+	vim.g.python_host_prog = "/usr/bin/python"
+	vim.g.python3_host_prog = "/usr/bin/python3"
+end
