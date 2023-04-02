2545f9e2462029b592518bbde2c00f8b0d7f2978
林大中 <lindazhong@lindazhongs-MacBook-Air.local>
2023-04-02 20:21:35 +0800

    add masson settings

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,4 +1,15 @@
-require("mason").setup() -- lsp, linter, formatter installer
+-- lsp, linter, formatter installer
+require("mason").setup({
+	-- - "prepend" (default, Mason's bin location is put first in PATH)
+	-- - "append" (Mason's bin location is put at the end of PATH)
+	-- - "skip" (doesn't modify PATH)
+	PATH = "prepend",
+	pip = {
+		-- upgrade pip in the virtual environment before installing packages.
+		upgrade_pip = false,
+	},
+})
+
 require("plugins.lsp.lspconfig")
 require("plugins.lsp.null-ls")
 require("plugins.lsp.dap")
