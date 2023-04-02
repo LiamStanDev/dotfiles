75967e9180ba99c7cd9ac570a2d5ab52de7cf112
林大中 <lindazhong@lindazhongs-MacBook-Air.local>
2023-04-02 20:20:41 +0800

    add new python lsp settings

--- nvim/.config/nvim/lua/plugins/lsp/settings/pylsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/settings/pylsp.lua
@@ -2,17 +2,28 @@ return {
 	settings = {
 		pylsp = {
 			plugins = {
+				-- style checking
 				pycodestyle = {
 					enabled = false,
 					maxLineLength = 100,
 				},
+				-- error linter
+				pyflakes = { enabled = false },
 				flake8 = {
 					enabled = true,
-					ignore = { "E402", "E266", "W503", "E501", "F403", "F405" },
+					ignore = { "E501", "E266" },
 				},
-				pyflakes = {
-					enabled = false,
+				-- code complexity linter
+				mccabe = {
+					enable = true,
+					threshold = 15,
 				},
+				-- code formatting
+				autopep8 = { enabled = false },
+				yapf = { endabled = true },
+				-- Completions and renaming
+				rope_autoimport = { enabled = true },
+				rope_completion = { enabled = true },
 			},
 		},
 	},
---
+++ nvim/.config/nvim/lua/plugins/lsp/settings/pyright.lua
@@ -0,0 +1,17 @@
+return {
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "strict",
+				-- diagnosticMode = "workspace",
+				autoSearchPaths = true, -- find "src" if no execution environments in config file
+				inlayHints = {
+					variableTypes = true,
+					functionReturnTypes = true,
+				},
+				logLevel = "Information",
+				autoImportCompletions = true,
+			},
+		},
+	},
+}
