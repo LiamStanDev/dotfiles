568629abf31b70dc98ff5dca811a1868052cd005
liam <geffc1454@gmail.com>
2023-04-05 23:51:15 +0800

    add gloabl register

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,45 @@
 -- the core plugins
 local g_configs = require("core")
+
 -- set color theme here
 g_configs.colorscheme = "onedark"
+
 -- whether to see navigator bar in the top
 g_configs.navigator_on = false
+
 -- set the logo. see ./lua/ui/style/header.lua
 g_configs.dashboard_logo = "AstronautSmall"
+
+-- register lsp server for auto completion
+g_configs.lsp_servers = {
+	"tsserver",
+	"omnisharp",
+	"html",
+	"jsonls",
+	"lua_ls",
+	"cssls",
+	"pylsp", -- python lsp
+	"bashls",
+}
+
+-- register formatting and diagnostics server
+g_configs.null_ls_servers = {
+	"prettier",
+	"black", -- python formatter
+	"stylua", -- lua formatter
+	"beautysh", -- bash formatter
+	"csharpier", -- csharp formatter
+	"eslint", -- js, ts linter
+	--"cspell", -- so annoying
+	--"flake8",
+	--"mypy",
+}
+
+-- register debuger server
+g_configs.dap_servers = {
+	"coreclr", -- csharp
+	"python",
+}
 
 -- setup all plugin
 require("plugins-setup")
