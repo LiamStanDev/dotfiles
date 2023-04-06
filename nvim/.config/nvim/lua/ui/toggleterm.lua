7632f06595f6e28865afbe8b7ac460e6da25d4d3
林大中 <lindazhong@lindazhongs-MacBook-Air.local>
2023-04-06 15:47:09 +0800

    add new header

--- nvim/.config/nvim/lua/ui/toggleterm.lua
+++ nvim/.config/nvim/lua/ui/toggleterm.lua
@@ -26,6 +26,7 @@ return function()
 	function _G.set_terminal_keymaps()
 		local opts = { noremap = true }
 		vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+		-- the following keybindings make the lazygit move very lagging
 		-- vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
 		-- vim.api.nvim_buf_set_keymap(0, "t", "kj", [[<C-\><C-n>]], opts)
 		vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
