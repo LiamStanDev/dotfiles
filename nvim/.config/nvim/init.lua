72331f527547b5e60a526780a43de6898380dc7c
liam <geffc1454@gmail.com>
2023-04-16 13:08:21 +0800

    add new feature from 1simple repo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -68,3 +68,18 @@ g_configs.dap_servers = {
 
 -- setup all plugin
 require("plugins-setup")
+
+if vim.g.neovide then
+	vim.g.neovide_input_use_logo = false -- true on macOS
+	vim.g.neovide_input_macos_alt_is_meta = true
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	vim.g.neovide_scale_factor = 1.0
+	vim.g.neovide_padding_top = 1
+	vim.g.neovide_padding_bottom = 1
+	vim.g.neovide_padding_right = 1
+	vim.g.neovide_padding_left = 1
+	vim.g.neovide_transparency = 0.8
+	vim.g.neovide_refresh_rate = 60
+	vim.g.neovide_refresh_rate_idle = 5 -- for not using
+	vim.g.neovide_remember_window_size = true
+end
