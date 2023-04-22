3635bccdac5142576567a0c9987b30be2a8fc59e
liam <geffc1454@gmail.com>
2023-04-22 11:39:16 +0800

    add XDG user dir environment variables

--- zsh/.config/zsh/enviroment.zsh
+++ zsh/.config/zsh/enviroment.zsh
@@ -1,8 +1,24 @@
 # key setting
 export NO_AT_BRIDGE=1
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=wayland
+export GDK_BACKEND="wayland,x11"
 # XDG
 export XDG_CURRENT_DESKTOP=Hyprland
 export XDG_SESSION_DESKTOP=Hyprland
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_RUNTIME_DIR="/run/user/$UID"
+# Userr dir
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_TEMPLATES_DIR="$HOME/Templates"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_VIDEOS_DIR="$HOME/Videos"
 # input source
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
