7bfa68cb9b3cf1bd235badb7ad62a26facc07e4b
liam <geffc1454@gmail.com>
2023-05-02 14:50:51 +0800

    [new] gtk theme config

---
+++ hyprland/.config/hypr/.theme.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'kora'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
