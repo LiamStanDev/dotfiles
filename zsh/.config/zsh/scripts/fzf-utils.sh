60567f875ab10054192864b78296191deccb9263
liam <geffc1454@gmail.com>
2023-05-27 00:30:02 +0800

    [update]

--- zsh/.config/zsh/scripts/fzf-utils.sh
+++ zsh/.config/zsh/scripts/fzf-utils.sh
@@ -9,18 +9,18 @@ fzf-open-editor-home() {
 }
 
 fzf-cd-here() {
-    local selected_file=$(du . | awk '{print $2}' | fzf)
+    local selected_file=$(du -S . | awk '{print $2}' | fzf)
     local directory=$(dirname "$selected_file")
     cd "$directory"
 }
 
 fzf-cd-home() {
-    local search_path=(~/.config ~/Documents ~/.local)
-    local directory=$(du ${search_path} | awk '{print $2}' | fzf)
+    local search_path=(~/.config ~/Documents ~/.local ~/dotfiles ~/.config/dotfiles)
+    local directory=$(du -S ${search_path} | awk '{print $2}' | fzf)
     cd "$directory"
 }
 
-alias se="fzf-open-editor-here"
-alias seh="fzf-open-editor-home"
-alias sd="fzf-cd-here"
-alias sdh="fzf-cd-home"
+# alias se="fzf-open-editor-here"
+alias se="fzf-open-editor-home"
+# alias sd="fzf-cd-here"
+alias sd="fzf-cd-home"
