6453cd38b8ac931742bd9eebd21db361fdfbe9f0
Liam Lin <geffc1454@gmail.com>
2023-06-01 16:47:43 +0800

    [fix] add macOS fzf uitil setup

--- zsh/.config/zsh/scripts/fzf-utils.sh
+++ zsh/.config/zsh/scripts/fzf-utils.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
 
-fzf-open-editor-here() {
-    find . -type f |  fzf | xargs -r $EDITOR 
-}
+if [[ $OSTYPE =~ ^linux ]]; then
 
-fzf-open-editor-home() {
-    find ~ -type f | fzf | xargs $EDITOR  
-}
+    fzf-open-editor-here() {
+        find . -type f |  fzf | xargs -r $EDITOR
+    }
 
-fzf-cd-here() {
-    local selected_file=$(du -S . | awk '{print $2}' | fzf)
-    local directory=$(dirname "$selected_file")
-    cd "$directory"
-}
+    fzf-open-editor-home() {
+        find ~ -type f | fzf | xargs -r $EDITOR
+    }
 
-fzf-cd-home() {
-    local search_path=(~/.config ~/Documents ~/.local ~/dotfiles ~/.config/dotfiles)
-    local directory=$(du -S ${search_path} | awk '{print $2}' | fzf)
-    cd "$directory"
-}
+    fzf-cd-here() {
+        local selected_file=$(du -S . | awk '{print $2}' | fzf)
+        local directory=$(dirname "$selected_file")
+        cd "$directory"
+    }
+
+    fzf-cd-home() {
+        local search_path=(~/.config ~/Documents ~/.local ~/dotfiles ~/.config/dotfiles)
+        local directory=$(du -S ${search_path} | awk '{print $2}' | fzf)
+        cd "$directory"
+    }
+fi
+
+if [[ $OSTYPE =~ ^darwin  ]]; then
+    fzf-open-editor-here() {
+        find . -type f | fzf | xargs -o $EDITOR
+    }
+
+    fzf-open-editor-home() {
+        find ~ -type f | fzf | xargs -o $EDITOR
+    }
+
+    fzf-cd-here() {
+        local selected_file=$(du -sm . | awk '{print $2}' | fzf)
+        local directory=$(dirname "$selected_file")
+        cd "$directory"
+    }
+
+    fzf-cd-home() {
+        local search_path=(~/.config ~/Documents ~/.local ~/dotfiles ~/.config/dotfiles)
+        local directory=$(find ${search_path} -d | fzf)
+        cd "$directory"
+    }
+fi
+
+
 
 # alias se="fzf-open-editor-here"
 alias se="fzf-open-editor-home"
