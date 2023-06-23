95d04e96b6767bc46d41ab0528d1c27df9685207
liam <geffc1454@gmail.com>
2023-06-23 14:59:31 +0000

    [new] eww bar

--- eww/.config/eww/scripts/search.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "--full" ]]; then
-    pkill rofi || rofi -show drun -theme ~/.config/rofi/launchers/type-3/style-3.rasi
-
-elif [[ "$1" == "--small" ]]; then
-    pkill rofi || rofi -show drun -theme ~/.config/rofi/launchers/type-1/style-6.rasi
-fi
